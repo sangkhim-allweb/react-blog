b0357590bb8ce14e2d74e3ba99b84680be732740
sangkhim <sangkhimpay@gmail.com>
2023-05-28 00:48:10 +0700

    fix api

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -11,7 +11,7 @@ const Create = () => {
         e.preventDefault();
         const blog = {title, body, author};
 
-        fetch('http://localhost:8080/blogs/', {
+        fetch('http://localhost:8080/blogs', {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(blog)
